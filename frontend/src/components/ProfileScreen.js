e12b61aedbb1a3cf81ff0757cb2a099a7e999fc6
sashank parmar <parmarsatvan@gmail.com>
2022-05-28 22:34:43 +0530

    PrivateRoute are done

--- frontend/src/components/ProfileScreen.js
+++ frontend/src/components/ProfileScreen.js
@@ -1,11 +1,23 @@
+import Cookies from "js-cookie";
 import React from "react";
+import { useHistory } from "react-router-dom"
+import { useAuth } from "../contexts/authContext";
 
 const ProfileScreen = () => {
+    const history = useHistory();
+    const { isAuthenticated, toggleAuth } = useAuth();
 
+    const logoutMethod = () => {
+        Cookies.remove("token");
+        Cookies.remove("avatar");
+        history.push("/home/all");
+        toggleAuth(false);
+    }
 
     return (
         <div>
             <h1>You are in Profile screen.</h1>
+            <button onClick={logoutMethod}> Log out </button>
         </div>
     )
 }
